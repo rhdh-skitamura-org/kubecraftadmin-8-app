94afa7442bc3f69a1e7c2115b89d02b3e8da465b
skitamur <skitamur@redhat.com>
2024-09-21 21:48:17 +0900

    fix

--- src/app/kubecraftadmin.go
+++ src/app/kubecraftadmin.go
@@ -141,8 +141,8 @@ func ReconcileMCtoKubeMob(p *mcwss.Player, clientset *kubernetes.Clientset, mobT
 				pods, _ := clientset.CoreV1().Pods(ns).List(context.TODO(), metav1.ListOptions{})
 
 				for _, pod := range pods.Items {
-					if !Contains(playerEntitiesMap[p.Name()], fmt.Sprintf("%s:pod:%s", pod.Namespace, pod.Name))  || pod.Status.Phase == v1.PodSucceeded || pod.Status.Phase == v1.PodFailed {
-						fmt.Printf(fmt.Sprintf("Player %s killed %s:pod:%s\n", p.Name(), pod.Namespace, pod.Name))
+					if !Contains(playerEntitiesMap[p.Name()], fmt.Sprintf("%s:pod:%s", pod.Namespace, pod.Name)) || pod.Status.Phase == v1.PodSucceeded || pod.Status.Phase == v1.PodFailed {
+						fmt.Printf(fmt.Sprintf("Player %s killed %s:pod:%s phase:%s\n", p.Name(), pod.Namespace, pod.Name, pod.Status.Phase))
 						clientset.CoreV1().Pods(pod.Namespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
 						playerUniqueIdsMap[p.Name()] = Remove(playerUniqueIdsMap[p.Name()], fmt.Sprintf("%s:pod:%s", pod.Namespace, pod.Name))
 					}
